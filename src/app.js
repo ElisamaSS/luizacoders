8b2a1f8630c73f83bd8b3148e860d482b1ba7c54
Elisama Santos <eli.sama15@hotmail.com>
2021-08-31 19:52:50 -0300

    consumindo rickandmortyapi

--- src/app.js
+++ src/app.js
@@ -1,22 +1,49 @@
-import express from 'express';
-import routes from './router';
-import './database';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-class App{
+import LoadImage from './img/giphy.webp';
+import LogoRick from './img/logo.png';
 
-    constructor(){
-        this.server = express();
-        this.middleware();
-        this.routes();
-    }
+export default function App(){
+  const [ data, setData ] = useState();
+  const [ isLoading, setIsload ] = useState(true);
 
-    middleware(){
-        this.server.use(express.json())
-    }
 
-    routes(){
-        this.server.use(routes);
-    }
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character').then(
+      res => {
+        setData(res.data.results);
+        setTimeout(() => {
+          setIsload(false);
+        }, 500)
+      }
+    )
+  }, []);
+
+  if(isLoading){
+    return (
+      <div>
+        <img src={LoadImage} alt="loading" />
+      </div>
+    )
+  }
+
+  return (
+    <div className="content">
+      <div className="container">
+
+        <div className="logo-content">
+          <img src={LogoRick} alt="Logo"  height="100px" width="auto"/>
+        </div>
+        <div className="cards-content">
+          { data.map( (item, index) => (
+            <div key={index} className="card">
+              <img src={item.image} alt={item.name} width="180px" height="auto"/>
+              <h4>{item.name}</h4>
+            </div>
+          )) }
+        </div>
+      </div>
+    </div>
+  )
 }
-
-export default new App().server
